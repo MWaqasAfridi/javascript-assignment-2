a5798f3af79a431ede55bfe3a4c1187c89621ca8
MWaqasAfridi <153609017+MWaqasAfridi@users.noreply.github.com>
2024-03-01 10:07:31 -0800

    Initial commit

---
+++ app.js
@@ -0,0 +1,213 @@
+// ============>Assignment 2===========>
+
+// ==========>chapter 4 legal and illegal===========>
+// Question no 1
+// var a,b,c;
+
+// Question no 2
+// ======>legal======>
+// var fullName;
+// var Age;
+// var _age;
+// var $age;
+// var full_Name;
+// var userName123;
+// var user123Name;
+
+// =====>illegal======>
+// var 1name;  (start me number ni askta)
+// var user-name;  (hyphens ni askta)
+// var first Name;  (space ni askta)
+// var var;  (variable name ka sat keyword ni laga sakty)
+// var 2*lastname;  (methematical or logical operator ni laga sakty variable ka sat
+//      matlb * / - % = == !== && || )
+
+// Question no 3 
+// document.write("<h1>Rules for naming JS variables</h1>")
+// document.write("variable must begin with a $name or _name and name"+"<br>");
+// document.write("variable name are case sensetive"+"<br>");
+// document.write("variable name should not be keywords");
+
+
+// ==========chapter 5 math expression (familiar operators)=============
+// Question no 1
+// var num1=3;
+// var num2=5;
+// var num=num1+num2;
+// document.write("Sum of 3 and 5 is"+" "+num);
+
+// Question no 2
+// var num1=33;
+// var num2=50;
+// var num=num1-num2;
+// document.write("Subtraction is ="+" "+num);
+
+// var num1=11;
+// var num2=2;
+// var num=num1*num2;
+// document.write("multiplication is ="+" "+num);
+
+// var num1=20000;
+// var num2=80;
+// var num=num1/num2;
+// document.write("Division is ="+" "+num);
+
+// var num1=15;
+// var num2=2;
+// var num=num1%num2;
+// document.write("modulus is ="+" "+num);
+
+
+// Question no 3
+// var meHoDeclare;
+// document.write("Value after variable declaration is"+" "+meHoDeclare+"<br>");
+// var a=5;
+// document.write("initial Value:"+" "+a+"<br>");
+// var b=++a;
+// document.write("Value after increment is:"+" "+b+"<br>");
+// var c=b+7;
+// document.write("Value after addition is:"+" "+c+"<br>");
+// var d=--c;
+// document.write("Value after decrement is:"+" "+d+"<br>");
+// var e=d%3;
+// document.write("The remainder is:"+" "+e+"<br>");
+
+
+// Question no 4
+// var oneMovieTicketCost=600;
+// var totalCost=600*5;
+// document.write("total cost to buy 5 tickets to a movie is"+" "+totalCost+"PKR");
+
+
+// Question no 5
+// document.write("<h1>Table of 12</h1>")
+// var table=12;
+// document.write("12*1="+" "+table*1+"<br>");
+// document.write("12*2="+" "+table*2+"<br>");
+// document.write("12*3="+" "+table*3+"<br>");
+// document.write("12*4="+" "+table*4+"<br>");
+// document.write("12*5="+" "+table*5+"<br>");
+// document.write("12*6="+" "+table*6+"<br>");
+// document.write("12*7="+" "+table*7+"<br>");
+// document.write("12*8="+" "+table*8+"<br>");
+// document.write("12*9="+" "+table*9+"<br>");
+// document.write("12*10="+" "+table*10);
+
+
+// Question no 6
+// var celcius=25;
+// var fahrenheit=(celcius*9/5)+32;
+// document.write("25°C is"+" "+fahrenheit+"°F"+"<br>");
+// var fahrenheit=70;
+// var celcius=(fahrenheit-32)*5/9;
+// document.write("25°F is"+" "+celcius+"°C");
+
+
+// Question no 7
+// document.write("<h1>Shopping Card</h1>")
+// var PriceOfItem1=650;
+// var orderedQuantityOfItem1=3;
+// var PriceOfItem2=100;
+// var orderedQuantityOfItem2=7;
+// var shippingCharges=100;
+// var totalCost=(PriceOfItem1*orderedQuantityOfItem1)
+// +(PriceOfItem2*orderedQuantityOfItem2)+shippingCharges;
+// document.write("Price of item 1 is"+" "+PriceOfItem1+"<br>");
+// document.write("Quantity of item 1 is"+" "+orderedQuantityOfItem1+"<br>");
+// document.write("Price of item 2 is"+" "+PriceOfItem2+"<br>");
+// document.write("Quantity of item 2 is"+" "+orderedQuantityOfItem2+"<br><br>");
+// document.write("Total cost of your order is"+" "+totalCost);
+
+
+// Question no 8
+// document.write("<h1>Marks Sheet</h1>")
+// var totalMarks=980;
+// var marksObtained=804;
+// var percentage=(marksObtained/totalMarks)*100;
+// document.write("Total marks:"+" "+totalMarks+"<br>");
+// document.write("Marks obtained:"+" "+marksObtained+"<br>");
+// document.write("Percentage:"+" "+percentage+"%");
+
+
+// Question no 9
+// document.write("<h1>Currency in PKR</h1>")
+// var US_dollars=10;
+// var saudiRiyals=25;
+// var convertPakistaniRupees=(US_dollars*104.80)+(saudiRiyals*28);
+// document.write("Total Currency in PKR:"+" "+convertPakistaniRupees);
+
+
+// Question no 10
+// var a=2;
+// var result=((a+5)*10)/2;
+// document.write("Final result is:"+" "+result);
+
+
+// Question no 11
+// document.write("<h1>Age Calculator</h1>")
+// var currentYear=2024;
+// var birthYear=2002;
+// var age=currentYear-birthYear;
+// document.write("Current Year:"+" "+currentYear+"<br>");
+// document.write("Birth Year:"+" "+birthYear+"<br>");
+// document.write("Your age is:"+" "+age);
+
+
+// Question no 12
+// var radius=20;
+// var circumferenceOfaCircle=2*3.142*radius;
+// var areaOfaCircle=3.142*radius*radius;
+// document.write("Radius of a Circle:"+" "+radius+"<br>")
+// document.write("The Circumference is:"+" "+circumferenceOfaCircle+"<br>");
+// document.write("the area is:"+" "+areaOfaCircle);
+
+
+// Question no 13
+// document.write("<h1>The Lifetime Supply Calculator</h1>")
+// var favouriteSnake="chips";
+// var currentAge=22;
+// var maximumAge=70;
+// var amountPerDay=2;
+// var yearsRemaining=maximumAge-currentAge;
+// var daysRemaining=yearsRemaining*365;
+// var total=daysRemaining*amountPerDay;
+// document.write("Favourite Snack:"+" "+favouriteSnake+"<br>");
+// document.write("Current age:"+" "+currentAge+"<br>");
+// document.write("Maximum Age:"+" "+maximumAge+"<br>");
+// document.write("Amount of snacks per day:"+" "+amountPerDay+"<br>");
+// document.write("you will Need"+" "+total+" "+favouriteSnake+" "+"to last you until the ripe old age of"+" "+maximumAge);
+
+
+
+
+// ===========>chapter 6 to 9 ============>
+// Question no 3
+// var name=prompt("Enter your name?")
+// alert("Hello,"+" "+name+" "+"! welcome to our website");
+
+
+// Question no 4
+// var table=prompt("Enter your table");
+// if(table===""){
+//     table=5;
+// }
+// document.write(table+" x 1 = "+table*1+"<br>");
+// document.write(table+" x 2 = "+table*2+"<br>");
+// document.write(table+" x 3 = "+table*3+"<br>");
+// document.write(table+" x 4 = "+table*4+"<br>");
+// document.write(table+" x 5 = "+table*5+"<br>");
+// document.write(table+" x 6 = "+table*6+"<br>");
+// document.write(table+" x 7 = "+table*7+"<br>");
+// document.write(table+" x 8 = "+table*8+"<br>");
+// document.write(table+" x 9 = "+table*9+"<br>");
+// document.write(table+" x 10 = "+table*10);
+
+
+
+
+
+
+
+
+
+
